f67c351ba23077c8fc4d65a3758ac857cb902e9a
KeikoMotomura <keikomotomuraapple@gmail.com>
2014-12-15 10:55:42 +0800

    Result画面完成

--- Appli-3/resultViewController.m
+++ Appli-3/resultViewController.m
@@ -27,71 +27,72 @@
     self.select_correctanswerNo = self.select_correctanswerNo;
     NSLog(@"正解数@%d", self.select_correctanswerNo);
     
+    
     switch (self.select_correctanswerNo) {
         case 0:
             self.resultLabel.text = @"ファイト！ ０問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"0correct.png"];
             break;
             
         case 1:
             self.resultLabel.text = @"ファイト！ １問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"1correct.png"];
             break;
             
         case 2:
             self.resultLabel.text = @"次は行ける！ ２問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"2correct.png"];
             break;
             
         case 3:
             self.resultLabel.text = @"次は行ける！ ３問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"3correct.png"];
             break;
             
         case 4:
             self.resultLabel.text = @"もう少し！ ４問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"4correct.png"];
             break;
             
         case 5:
             self.resultLabel.text = @"なかなか！ ５問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"5correct.png"];
             break;
             
         case 6:
             self.resultLabel.text = @"なかなか！ ６問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"6correct.png"];
             break;
             
         case 7:
             self.resultLabel.text = @"おめでとう！ ７問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"7correct.png"];
             break;
             
         case 8:
             self.resultLabel.text = @"おめでとう！ ８問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"8correct.png"];
             break;
             
         case 9:
             self.resultLabel.text = @"おめでとう！ ９問正解";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"9correct.png"];
             break;
             
         case 10:
             self.resultLabel.text = @"パーフェクト！！";
             self.resultLabel.textColor = [UIColor blueColor];
-            self.resultImage.image = [UIImage imageNamed:@"1correct.jpg"];
+            self.resultImage.image = [UIImage imageNamed:@"10correct.png"];
             break;
             
             
